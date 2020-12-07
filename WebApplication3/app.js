1ca5862531da2e16f74b3d148218fcef3169ba74
SOLCO\10011345 <srikanth.ganesan@itelligence.org>
2020-12-07 09:33:55 +0530

    Added Web Application

---
+++ WebApplication3/app.js
@@ -0,0 +1,13 @@
+import React, {component} from 'React'
+class React extends component{
+    render(){
+        console.log(this.props);
+        const{name,age}=this.props;
+        return
+        <div className="react">
+            <div>className {this.props.name}</div>
+            <div>age:{this.props.age}</div>
+            <div>belt:{this.props</div>
+        
+    }
+}
